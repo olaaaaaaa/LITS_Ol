de3e0135a5ab444e85ba77bbbe83834bfdd13473
olaaaaaaa <oljysja@gmail.com>
2016-05-19 20:54:01 +0300

    first commit by css key

---
+++ calculator.rb
@@ -0,0 +1,17 @@
+#string => " 5 * 3 + 8 / 4 "
+#puts string.gsub! /\s/, ''
+
+
+# puts " 5 * 3 + 8 / 4 ".gsub! /\s/, ''
+
+class Calculator
+
+  # def self.add(a, b)
+  #  return a+b
+  # end
+
+  def subtract(a, b)
+   a-b
+  end
+
+end
